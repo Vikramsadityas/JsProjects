5a529602a573cdcd30e0e42eca2182d76e20e8fa
Vikram13shri <vikramsadityas@gmail.com>
2024-07-01 14:25:00 +0530

    BgChanger

---
+++ bgChanger/main.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.body;
+buttons.forEach(function (button) {
+  button.addEventListener('click', function (e) {
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = 'grey';
+        break;
+      case 'white':
+        body.style.backgroundColor = 'white';
+        break;
+      case 'blue':
+        body.style.backgroundColor = 'blue';
+        break;
+      case 'yellow':
+        body.style.backgroundColor = 'yellow';
+        break;
+      default:
+        alert('Please select Box Correctly');
+    }
+  });
+});
